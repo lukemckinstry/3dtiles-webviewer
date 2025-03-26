b94de2aea811be1915499ba5c5d32f01de589eb8
lukemckinstry <lukemckinstry@gmail.com>
2025-03-26 15:10:49 -0400

    gltf transform wip

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Button } from '@itwin/itwinui-react/bricks';
+import { WebIO } from '@gltf-transform/core';
+
+
 
 let Header = () => {
-    const handleClick = () => {
+    const io = new WebIO();
+    
+    const handleClick = async () => {
+        const document = await io.read('data/SanFran_Street_level_Ferry_building/0/0.glb'); // → Document
+        console.log("document ", document)
+
     }
     return (
         <>
@@ -14,7 +22,7 @@ let Header = () => {
                     {"Header"}
                 </div>
                 <div className="header-button-container">
-                        <Button onClick={handleClick}> Login</Button>
+                        <Button onClick={handleClick}> Load Tileset</Button>
                 </div>
             </div>
         </>
